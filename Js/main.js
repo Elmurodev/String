886b91dd7e24d35f48719c797adbc7bec3424261
elmurodev <Elmurod0298@gmail.com>
2021-11-26 22:19:02 +0500

    String commit

---
+++ Js/main.js
@@ -0,0 +1,23 @@
+var numbers = ([2,3,5,6,7,8,9,10,12,13,]);
+numbers.forEach(thisFunction)
+
+function thisFunction(item, i, array) {
+    array [i] = item **2;
+    console.log(item, i, array);
+}
+
+
+var name2 = "O'zbekiston";
+
+console.log(name2.split(""));
+
+var result = name2.split("")
+
+console.log(result[2][0]);
+
+
+
+const text = "O'zbekiston"
+const reverse= 
+text.split("").reverse().join("")
+console.log(reverse);
